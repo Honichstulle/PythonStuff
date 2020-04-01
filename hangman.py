7688ee8968ccac0fbce9edd81ee693ffbc2f7a0d
Honichstulle <ac3ofsp4des893@gmail.com>
2020-04-01 16:35:47 +0200

    Lets Go

---
+++ hangman.py
@@ -0,0 +1,55 @@
+import random
+
+
+#Funktion die prüft ob der eingegebene Buchstabe im Wort vorhanden ist
+def check_letter(wd,wl,array):
+
+ wrong = 0
+ strike = 0
+ counter = 0
+ guess_array = array
+
+ while strike < 11:
+    guess = input("Rate einen Buchstaben: ")
+    for i in wd:
+        if guess == i:
+            print("true")
+            counter = counter + 1
+            guess_array.insert(counter,i)
+            del guess_array[counter - 1]
+
+        else:
+            print("false")
+            wrong = wrong + 1
+            counter = counter + 1
+
+    if wrong == wl:
+        strike = strike + 1
+        print(strike)
+        print(guess_array)
+        wrong = 0
+        counter = 0
+    else:
+        print("Correct!")
+        print(guess_array)
+        wrong = 0
+        counter = 0
+
+
+
+#Liste die alle verfügbaren Wörter enthält
+wordlist = ['feuerwehr', 'polizei', 'rettungswagen']
+#Funktion um ein zufälliges Wort aus der Liste auszuwählen
+word = random.choice(wordlist)
+#Länge des aktuellen Wortes
+wordlength = len(word)
+#Erstellen des Arrays, welches später die richtig geratenen Buchstaben enthält
+guess_array = []
+for i in word:
+    guess_array.append("-")
+
+
+
+print("Das gesuchte Wort hat " + str(wordlength) + " Buchstaben")
+
+check_letter(word,wordlength,guess_array)
